b155a3d2892d466e2827e6332800991f79285e7a
Jenishgul <jenishgul49@gmail.com>
2024-12-02 16:00:46 +0600

    auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Clone from "@/components/Clone";
+import SignIn from "@/components/Signln";
 import TodoistClone from "@/components/TodoistClone";
 
 export default function Home() {
   return (
     <div>
       <TodoistClone />
-        3x Comp(Items, List, FORM)
-      {/* <Clone/> */}
+      <SignIn/>
     </div>
   );
 }
