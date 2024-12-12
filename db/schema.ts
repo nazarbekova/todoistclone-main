54162013b6ad3d03ad9ce6e35d1ee78aa3cd5d19
Jenishgul <jenishgul49@gmail.com>
2024-12-12 14:37:03 +0600

    asd

--- db/schema.ts
+++ db/schema.ts
@@ -3,5 +3,5 @@ import { integer, text, boolean, pgTable } from "drizzle-orm/pg-core";
 export const todo1 = pgTable("todo1", {
   id: integer("id").primaryKey(),
   text: text("text").notNull(),
-  done: boolean("done").default(false).notNull(),
+  completed: boolean("done").default(false).notNull(),
 });
