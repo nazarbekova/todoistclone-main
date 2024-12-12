3826d6705e5d843e9ac921a370ac670526971198
Jenishgul <jenishgul49@gmail.com>
2024-12-12 14:45:53 +0600

    test

--- drizzie.config.ts
+++ drizzie.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from "drizzle-kit";
 config({ path: ".env" });
 
 export default defineConfig({
-  schema: "./db/schema.ts",
-  out: "./db/migrations",
+  schema: "db/schema.ts",
+  out: "db/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
